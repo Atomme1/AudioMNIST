a55467d893c625e894e319a4bdeb600bcb5be38a
Atomme1 <tristan@lataniere.net>
2022-12-07 17:55:39 +0100

    update

--- create_model_DeepSpeech2.py
+++ create_model_DeepSpeech2.py
@@ -4,6 +4,44 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
+# An integer scalar Tensor. The window length in samples.
+frame_length = 256
+# An integer scalar Tensor. The number of samples to step.
+frame_step = 160
+# An integer scalar Tensor. The size of the FFT to apply.
+# If not provided, uses the smallest power of 2 enclosing frame_length.
+fft_length = 384
+
+# The set of characters accepted in the transcription.
+characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
+
+
+def get_characters():
+    characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
+    return characters
+
+
+# Mapping characters to integers
+char_to_num = keras.layers.StringLookup(vocabulary=characters, oov_token="")
+
+
+def get_char_to_num(characters):
+    char_to_num = keras.layers.StringLookup(vocabulary=characters, oov_token="")
+    return char_to_num
+
+
+# Mapping integers back to original characters
+num_to_char = keras.layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+
+def get_num_to_char():
+    num_to_char = keras.layers.StringLookup(
+        vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+    )
+    return num_to_char
+
 
 def build_model(input_dim, output_dim, rnn_layers=5, rnn_units=128):
     """Model similar to DeepSpeech2."""
@@ -66,10 +104,37 @@ def build_model(input_dim, output_dim, rnn_layers=5, rnn_units=128):
     return model
 
 
-# Get the model
-model = build_model(
-    input_dim=fft_length // 2 + 1,
-    output_dim=char_to_num.vocabulary_size(),
-    rnn_units=512,
-)
-model.summary(line_length=110)
+def CTCLoss(y_true, y_pred):
+    # Compute the training-time loss value
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+    loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+    return loss
+
+# A utility function to decode the output of the network
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    # Iterate over the results and get back the text
+    output_text = []
+    for result in results:
+        result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
+        output_text.append(result)
+    return output_text
+
+def decode_predictions(pred):
+    input_len = np.ones(pred.shape[0])
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+    # Iterate over the results and get back the text
+    output_text = []
+    for result in results:
+        result = tf.strings.reduce_join(num_to_char(result)).numpy().decode("utf-8")
+        output_text.append(result)
+    return output_text
